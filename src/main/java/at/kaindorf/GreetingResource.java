95b495385e18e920e0b90306ee1c79056e1efd98
filmad19 <filmad19@htl-kaindorf.at>
2023-03-24 17:15:52 +0100

    initial

--- src/main/java/at/kaindorf/GreetingResource.java
+++ src/main/java/at/kaindorf/GreetingResource.java
@@ -11,6 +11,6 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello frrfom RESTEasy Reactive";
+        return "Hello frrfodsfm RESTEasy Reactive";
     }
 }

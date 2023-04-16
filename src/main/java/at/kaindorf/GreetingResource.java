5a97b6061d2ee77757614d7eb7add8257c35bc41
filmad19 <filmad19@htl-kaindorf.at>
2023-04-16 19:08:14 +0200

    test weil wer feature branch gelöscht hat :(

--- src/main/java/at/kaindorf/GreetingResource.java
+++ src/main/java/at/kaindorf/GreetingResource.java
@@ -11,6 +11,6 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello frrffdm RESTEasy Reactive";
+        return "Hello frrffdm RESTEasyd Reactive";
     }
 }
